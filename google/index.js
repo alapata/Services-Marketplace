836f0d48ff31b9bf0895edd7ffd7f6b0d0713f15
eamachine <eamachine@gmail.com>
2020-08-19 11:27:13 -0500

    google partial

--- google/index.js
+++ google/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+  let redis     = require('redis'),
+  /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
+  client = redis.createClient({
+    port: 14542,
+    host: 'redis-14542.c8.us-east-1-4.ec2.cloud.redislabs.com',
+    password: 'VREdWqF5PJBYj4jRZhrVvFBdcQaWpL3k',
+  });
+  
+  client.get('store',function(err,value) {
+        if (err) {
+          throw err;
+        } else {
+          response.status(200).send(value);
+        }
+      }
+    );
 };
 
 exports.event = (event, callback) => {
