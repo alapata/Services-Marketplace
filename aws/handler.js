5db7590f1bec17b7244bda0dd25d54d845c5121c
carlos <eamachine@gmail.com>
2020-08-23 21:23:59 -0500

    aws

--- aws/handler.js
+++ aws/handler.js
@@ -1,17 +1,37 @@
 'use strict';
 
+async function getStores(client, keys) {
+  var stores = new Array();
+
+  for(let i = 0, len = keys.length; i < len; i++) {
+    stores[i] = await client.get(keys[i]).then();
+  }
+  return stores;
+}
+
+
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  
+  
+  let redis = require('promise-redis')(),
+  
+  client = redis.createClient({
+    port: 16180,
+    host: 'redis-16180.c238.us-central1-2.gce.cloud.redislabs.com',
+    password: 'Eaf5svuXjw8LBuJuCkrFggGKvhThIM4U',
+  });
+
+  client.keys('*', async function (err, keys) {
+    if (err) {
+      throw err;
+    } else {
+      stores = await getStores(client, keys);      
+      return {
+        statusCode: 200,
+        body: JSON.stringify(stores),
+      };      
+    }
+});  
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
