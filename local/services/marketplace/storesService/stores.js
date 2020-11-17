6451f74e4a9cd62e48623783f5dce4ceccffa54c
Davras <davidloaiza13@gmail.com>
2020-11-17 15:26:41 -0500

    Implementación de jwt en peticiones get de leer y crear tiendas stores

--- local/services/marketplace/storesService/stores.js
+++ local/services/marketplace/storesService/stores.js
@@ -1,22 +1,38 @@
 module.exports = function(req, res) {
     console.log('Entró a storesService/stores.js');
 
-    let redis = require('redis'),
+    const jwt = require('jsonwebtoken');
+    const token = req.headers['authorization'];
+    const tokenKey = require('../../helpers/constants');
 
-        client = redis.createClient({
-            port: 16180,
-            host: 'redis-16180.c238.us-central1-2.gce.cloud.redislabs.com',
-            password: 'Eaf5svuXjw8LBuJuCkrFggGKvhThIM4U',
-        });
-
-    client.keys('*', function(err, keys) {
+    jwt.verify(token, tokenKey.tokenKey, (err, decoded) => {
         if (err) {
-            throw err;
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'token inválido',
+                err
+            });
         } else {
-            for (var i = 0, len = keys.length; i < len; i++) {
-                console.log(keys[i]);
-            }
-            res.status(200).json({ keys });
+            let redis = require('redis'),
+
+                client = redis.createClient({
+                    port: 16180,
+                    host: 'redis-16180.c238.us-central1-2.gce.cloud.redislabs.com',
+                    password: 'Eaf5svuXjw8LBuJuCkrFggGKvhThIM4U',
+                });
+
+            client.keys('*', function(err, keys) {
+                if (err) {
+                    throw err;
+                } else {
+                    for (var i = 0, len = keys.length; i < len; i++) {
+                        console.log(keys[i]);
+                    }
+                    res.status(200).json({ keys });
+                }
+            });
         }
     });
+
+
 }
